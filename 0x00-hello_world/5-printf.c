481e2768f9e9d8a227b1aa7df2e862206da86570
Kaylee Dao <kayleed2@gmail.com>
2021-09-27 10:20:49 -0700

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - Uses printf to print a string
+ * Description: Uses printf to pring with proper grammar, but the outcome is a
+ * piece of art, followed by a new line.
+ * Return: Return value should be 0 indicating success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
