098b0404d03cd343cd5e22284b0bbf3c2fe61cdb
Kaylee Dao <kayleed2@gmail.com>
2021-09-27 10:04:08 -0700

    Make betty happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-/* Standard Output using puts */
+/**
+* main - This function prints on the Standard Output using puts
+* Description: Using char variable and puts to print string
+* Return: The return value should be 0
+*/
+
 int main(void)
 {
-char _putex[] = "Programming is like building a multilingual puzzle";
+char _putex[] = "\"Programming is like building a multilingual puzzle";
 
 puts(_putex);
 return (0);
