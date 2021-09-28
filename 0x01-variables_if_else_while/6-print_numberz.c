d28fc6d87839eca7b6c0eabf6122fb70815e725d
Kaylee Dao <kayleed2@gmail.com>
2021-09-28 15:27:11 -0700

    Use ASCII equivalent

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - Print alphabet
+ * Description: prints the alphabet in lowercase, followed by a new line
+ * Return: Return value should be 0 indicating success
+*/
+int main(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
