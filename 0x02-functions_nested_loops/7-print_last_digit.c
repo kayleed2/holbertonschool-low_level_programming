32412576e7cb3753f1b1eb773e753f8d49f04d1f
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 13:25:19 -0700

    Use remainder

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+*  print_last_digit - Prints last digit of num
+* @i: Value to be taken last digit from
+* Description: The above
+* Return: Return value should be last digit of i indicating success
+*/
+
+int print_last_digit(int i)
+{
+	int result;
+
+	result = i % 10;
+	return (result);
+}
