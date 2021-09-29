504fd8b9d8162b1612cbec9280f02d1cd7082483
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 11:21:24 -0700

    Nested If

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+# include "main.h"
+/**
+* main - Calls alphabet function written in main.h
+* Description: The above
+* Return: Return value should be _putchar indicating success
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+	n = 1;
+	i = 'a';
+	while (n <= 10 && i <= 'z')
+	{
+		_putchar(i);
+		i++;
+		if (i > 'z')
+		{
+			_putchar('\n');
+			i = 'a';
+			n++;
+		}
+	}
+	return;
+}
