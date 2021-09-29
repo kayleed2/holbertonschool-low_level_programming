8b63b1f23cfbabacf095a72c08474661102ce207
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 12:36:42 -0700

    Result and _putchar

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+* print_sign - Checks if variable is a letter
+* @n: Value to be determined as less than, eq, or > 0
+* Description: The above
+* Return: Return value should be 0 or 1
+*/
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+		result = 1;
+	_putchar('+');
+	else if (c == 0)
+		result = 0;
+	_putchar('0');
+	else if (c < 0)
+		result = -1;
+	_putchar('-');
+	return (result);
+}
