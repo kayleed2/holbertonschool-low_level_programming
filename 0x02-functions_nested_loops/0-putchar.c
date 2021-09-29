b0f3c4da9d314d743c9e11b2d8e1ab602cd93592
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 07:59:58 -0700

    Using _putchar multiple times

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+# include "main.h"
+/**
+* main - Uses _putchar to print out _putchar
+* Description: The above
+* Return: Return value should be _putchar indicating success
+*/
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
