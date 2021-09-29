6742fdfa9afc2491ffa9dc1ff3b58323a171ab64
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 09:24:55 -0700

    Loop i, return void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+# include "main.h"
+/**
+* main - Calls alphabet function written in main.h
+* Description: The above
+* Return: Return value should be _putchar indicating success
+*/
+
+void print_alphabet(void)
+{
+	int i;
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+	i++;
+	}
+	_putchar('\n');
+	return;
+}
