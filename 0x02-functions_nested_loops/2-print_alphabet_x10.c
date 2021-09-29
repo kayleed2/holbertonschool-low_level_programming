d5e87ae665c75a5c7d8f1eca8ff94a3e4fc462f4
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 11:54:16 -0700

    Changing Comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 # include "main.h"
 /**
-* main - Calls alphabet function written in main.h
+* print_alphabet_x10 - Calls alphabet function written in main.h
 * Description: The above
 * Return: Return value should be _putchar indicating success
 */
@@ -9,8 +9,10 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int n;
+
 	n = 1;
 	i = 'a';
+
 	while (n <= 10 && i <= 'z')
 	{
 		_putchar(i);
@@ -22,5 +24,4 @@ void print_alphabet_x10(void)
 			n++;
 		}
 	}
-	return;
 }
