a0d802c68cd20b9298fa2162babd7929ff3c3a4c
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 13:08:20 -0700

    Multiplying by -

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+# include "main.h"
+/**
+* _abs - Calls alphabet function written in main.h
+* @i: Value to convert into it's absolute
+* Description: The above
+* Return: Return value should be _putchar indicating success
+*/
+
+int _abs(int i)
+{
+	int i;
+	int result;
+
+	if (i >= 0)
+	{
+		result = i
+			}
+		else if (i < 0)
+		{
+			i = i * -1;
+			result = i;
+		}
+	return (result);
+}
