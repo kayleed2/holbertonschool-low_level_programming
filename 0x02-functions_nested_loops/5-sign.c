e322fd48a5bdaa06b496e9aa057fe0220e9f7816
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 12:38:46 -0700

    Changed var name

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,13 +11,19 @@ int print_sign(int n)
 	int result;
 
 	if (n > 0)
+	{
 		result = 1;
 	_putchar('+');
-	else if (c == 0)
+	}
+	else if (n == 0)
+	{
 		result = 0;
 	_putchar('0');
-	else if (c < 0)
+	}
+	else if (n < 0)
+	{
 		result = -1;
 	_putchar('-');
+	}
 	return (result);
 }
