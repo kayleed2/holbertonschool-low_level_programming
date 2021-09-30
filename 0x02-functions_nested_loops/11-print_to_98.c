cd7bdb6819cfd45f4e3d9faadb77e11de322ea3c
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 18:34:40 -0700

    If-for-else-for

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,27 +9,23 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 	printf("%d", n);
 	putchar(',');
 	putchar(' ');
 	n--;
-	for (n = 98;;)
-	printf("%d", n);
 	}
-	else if (n < 98)
+	else if (n <= 98)
 	{
 		printf("%d", n);
 		putchar(',');
 		putchar(' ');
 		n++;
-		for (n = 98;;)
-			printf("%d", n);
 	}
 	else if (n == 98)
 	{
-	printf("%d", n);
+		printf("98");
 	}
 
 }
