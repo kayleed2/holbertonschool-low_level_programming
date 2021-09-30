3aa50de19a50cf41b223106b0245ec7fb1def6d7
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 19:12:00 -0700

    Moved == 98 first

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,12 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n >= 98)
+	{
 		while (n != 98)
 		{
 			printf("%d", n);
@@ -19,18 +24,17 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d\n", n);
 		}
+	}
 	else if (n <= 98)
+	{
 		while (n != 98)
 		{
-		printf("%d", n);
-		putchar(',');
-		putchar(' ');
-		n++;
-		if (n == 98)
-			printf("%d\n", n);
+			printf("%d", n);
+			putchar(',');
+			putchar(' ');
+			n++;
+			if (n == 98)
+				printf("%d\n", n);
 		}
-	else if (n == 98)
-	{
-		printf("%d\n", n);
 	}
 }
