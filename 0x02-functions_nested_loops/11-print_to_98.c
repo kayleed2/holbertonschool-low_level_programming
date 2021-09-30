dab8be6efd8ade140858892abfae03aaeed392c1
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 18:10:00 -0700

    Changed order of statements

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,16 @@
 
 void print_to_98(int n)
 {
-if (n <= 98)
-{
+	if (n == 98)
+	printf("%d", n);
+	else if (n > 98)
 	printf("%d", n);
 	putchar(',');
 	putchar(' ');
-	n++;
-}
-else if (n >= 98)
-{
-printf("%d", n);
-putchar(',');
-putchar(' ');
-n--;
-}
-else if (n == 98)
-{
+	n--;
+	else
 	printf("%d", n);
-}
+	putchar(',');
+putchar(' ');
+n++;
 }
