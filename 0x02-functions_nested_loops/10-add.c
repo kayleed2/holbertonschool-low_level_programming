0c039ccff78422bc37d39c893c8714cdacbcd856
Kaylee Dao <kayleed2@gmail.com>
2021-09-29 17:17:11 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+*  add - adds two integers and returns the result
+* @x: Value to be added
+* @y: Value to be added
+* Description: The above
+* Return: Return value should be value of last digit of i indicating success
+*/
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
