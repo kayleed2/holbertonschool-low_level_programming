491032e61634b9fbe2a0e60d6391122cb57d2be6
Kaylee Dao <kayleed2@gmail.com>
2021-10-04 09:51:53 -0500

    Print n underscores

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+# include "main.h"
+/**
+*  print_line - Prints _ n times
+* @n: Number of times to print _
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+		if (n < 0)
+		{
+			_putchar('\n');
+		}
+	}
+}
