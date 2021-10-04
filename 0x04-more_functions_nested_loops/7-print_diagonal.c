27805be2245034a0c161ec43297e799ae5a6fba9
Kaylee Dao <kayleed2@gmail.com>
2021-10-04 12:00:20 -0500

    Looping spaces

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+# include "main.h"
+/**
+* print_diagonal - Prints / n times
+* @n: Number of times to print
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+void print_diagonal(int n)
+{
+	int x;
+	int sp;
+
+	if (n > 0)
+	{
+		for(x = 0; x < n; x++)
+		{
+			for (sp = 1; sp <= x; sp++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
+	else
+	{
+		_putchar('\n');
+	}
+}
