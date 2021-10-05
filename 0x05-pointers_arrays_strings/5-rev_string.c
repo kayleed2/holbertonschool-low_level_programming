9cf0cb3f004a0a9e751d408c4565ee4c44e538ee
Kaylee Dao <kayleed2@gmail.com>
2021-10-05 17:29:33 -0500

    Print Both Loops

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,11 +21,15 @@ void rev_string(char *s)
 	_putchar('\n');
 	i--;
 	s--;
-	while (i > 0)
+	while (i >= 0)
 	{
 		_putchar(*s);
 		i--;
 		s--;
+		if (i == 0 && *s == '\0')
+		{
+			break;
+		}
 	}
 	_putchar('\n');
 }
