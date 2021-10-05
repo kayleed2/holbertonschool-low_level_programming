0438b241e10b31175f32fb2b265056dc759dba20
Kaylee Dao <kayleed2@gmail.com>
2021-10-05 10:15:11 -0500

    Assign new value *n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+* reset_to_98 - Prints restates var value
+* @*n: Pointer to change int
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
