2e50ae3644a9bf313b97665e585da48b08a8ce7a
Kaylee Dao <kayleed2@gmail.com>
2021-10-05 10:42:55 -0500

    If *s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+* _strlen - Prints restates var value
+* @s: String to determine length
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(++s));
+}
+
