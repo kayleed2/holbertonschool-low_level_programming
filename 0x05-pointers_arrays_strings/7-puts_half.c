aae3cdb4c3ca4f543bb3072470e0427fc7376ac7
Kaylee Dao <3597@holbertonschool.com>
2021-10-06 12:42:36 -0700

    Copy

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,6 +12,7 @@ char *copy = str;
 int i;
 int len;
 int ci;
+int n;
 
 i = 0;
 ci = 0;
@@ -25,10 +26,17 @@ while (*copy != '\0')
 {
 ci++;
 copy++;
-if (ci > len / 2)
+if (len % 2 == 0)
+{
+while (ci >= len / 2)
 {
 _putchar(*copy);
 }
+else
+{
+while (ci >= n)
+{
+_putchar(*copy);
 }
 _putchar('\n');
 }
