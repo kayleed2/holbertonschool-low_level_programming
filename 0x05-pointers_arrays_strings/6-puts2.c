08a16d9a066c32919a0d99f805ab83673a2418b9
Kaylee Dao <kayleed2@gmail.com>
2021-10-06 11:30:47 -0500

    While If

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+# include "main.h"
+/**
+* puts2 - prints every other character
+* @str: String to be printed
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+void puts2(char *str)
+{
+  while (*str)
+    {
+      _putchar(*str);
+      str++;
+      if (*str != '0')
+      {
+        str++;
+      }
+    }
+    _putchar('\n');
+}
