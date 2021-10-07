6cf4f2ad571448120e054a2069ed5d97d120ec68
Kaylee Dao <3597@holbertonschool.com>
2021-10-06 18:55:29 -0700

    Betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,26 +10,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int len;
 
 	dest = src;
 
 	for (i = 0; src[0] <= '\0'; i++)
 	{
-		if (src[i] == '\0')
-			{
-			break;
-			}
-			src++;
-	}
-	len = i;
-	for (i = 0; dest[i] <= len; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		dest++;
+		dest = src;
 	}
 	return (dest);
 }
