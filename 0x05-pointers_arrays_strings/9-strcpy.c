a5c8e015df532e9a830bf21d6269a6f06acb00bb
Kaylee Dao <3597@holbertonschool.com>
2021-10-06 17:20:29 -0700

    dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+# include "main.h"
+/**
+* _strcpy - prints number of values
+* @dest: Pointer to buffer
+* @src: Pointer to string
+* Description: The above
+* Return: Return value is pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	dest = src;
+
+	for (i = 0; src[0] <= '\0'; i++)
+	{
+		src++;
+	}
+	len = i;
+	for (i = 0; dest[i] <= len; i++)
+	{
+		dest++;
+	}
+	return(dest);
+}
