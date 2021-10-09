f2a57252ae8971c189e051b6c22232759e878277
Kaylee Dao <3597@holbertonschool.com>
2021-10-09 07:54:43 -0700

    Gcc?

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i;
 
-	if (*s1 - *s2 == 0)
-		return (0);
-	else
-		return (*s1 - *s2);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			if (*s1 - *s2 == 0)
+			{
+				return (0);
+			}
+			if (*s1 < *s2)
+			{
+				return (*s1 - *s2);
+			}
+			if (*s1 > *s2)
+			{
+				return (*s1 - *s2);
+			}
+		}
+	}
 }
