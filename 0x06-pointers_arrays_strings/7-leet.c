7fa6b2231ad9043029d0ff2540776afe69602031
Kaylee Dao <3597@holbertonschool.com>
2021-10-10 12:26:56 -0700

    Careful with [i]

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+# include "main.h"
+/**
+* cap_string - Turns string into uppercase
+* @a: String to be converted
+* Description: The above
+* Return: Return value is pointer to dest
+*/
+
+char *leet(char *a)
+{
+	int i;
+	int x;
+	char cap[5] = {'A','E','O','T','L'};
+	char low[5] = {'a','e','o','t','l'};
+	char num[5] = {'4', '3', '0', '7', '1'};
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		for (x = 0; x < 5; x++)
+		{
+			if (a[i] == cap[x] || a[i] == low[x])
+			{
+				a[i] = num[x];
+			}
+		}
+		i++;
+	}
+	return (a);
+}
