ebc9755cb6a76ee7c7776f9258979ec25a2903c2
Kaylee Dao <3597@holbertonschool.com>
2021-10-12 09:17:52 -0700

    Can't increment inside call

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+* _print_rev_recursion - Turns string into uppercase
+* @s: String to be printed in reverse
+* Description: Puts through recursion
+* Return: Return value is void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
