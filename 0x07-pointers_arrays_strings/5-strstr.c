d24ba15e6ad3746fde2b01420d857f12daa75b0b
Kaylee Dao <3597@holbertonschool.com>
2021-10-13 08:20:47 -0700

    Add if Needle is '\0'

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,8 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 
 	for (i = 0; needle[i] != '\0'; i++)
+		if (*needle == '\0')
+			return ('\0');
 	while (*haystack != '\0')
 	{
 		if (*haystack == *needle)
