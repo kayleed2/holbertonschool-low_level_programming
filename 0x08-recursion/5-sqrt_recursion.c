6719210d945bb1aa70d651e870aa99103ea6473d
Kaylee Dao <3597@holbertonschool.com>
2021-10-13 06:46:30 -0700

    New function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,13 @@
 # include "main.h"
 /**
-* _sqrt_recursion - Returns square root
-* @n: Value to find square root
-* Description: Value of square root through recursion
-* Return: Return value is void
-*/
+ *findsqr - takes _sqrt_recursion n and finds the square root if possible
+ *@n: Number to determine if square root
+ *@i: Number to multiply by itself then incr
+ *Return: -1 or square root
+ */
 
-int _sqrt_recursion(int n)
+int findsqr(int n, int i)
 {
-	int i;
-
-	i = n % 1;
 	if (i * i == n)
 	{
 		return (i);
@@ -19,5 +16,20 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (n);
+	return (findsqr(n, i + 1));
+}
+/**
+* _sqrt_recursion - Returns square root
+* @n: Value to find square root
+* Description: Value of square root through recursion
+* Return: Return value is square or -1
+*/
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	i = 1;
+
+	return (findsqr(n, i));
 }
