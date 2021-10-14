e2c1ba699b694355917273a4e9e65f219581cf3c
Kaylee Dao <3597@holbertonschool.com>
2021-10-14 15:14:20 -0700

    Print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include "main.h"
+/**
+ * main - This program prints number of arguments
+ * Return - 0 for success
+ */
+
+int main (int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
