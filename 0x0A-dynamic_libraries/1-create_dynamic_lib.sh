6f533c1c1393ef80ce0cac10fa5953b2bc1c5080
Kaylee Dao <3597@holbertonschool.com>
2021-10-17 06:06:51 -0700

    Fix grammar error

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Creates dynamic library from c files in .
-gcc ./*.c -c -Wall -pedantic - Werror -Wextra -fpic |
+gcc ./*.c -c -Wall -pedantic -Werror -Wextra -fpic |
 gcc *.o -shared -o liball.so
 
