bf53226e0d624da83da4cec8a1398c14587ef90b
Kaylee Dao <3597@holbertonschool.com>
2021-10-17 12:44:13 -0700

    Betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,13 @@
 # include <stdlib.h>
 # include <stdio.h>
-# include "main.h"
 /**
  * main - This program multiplies arguments
- * Return - 0 for success, Error for more than 2 args
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: 0 for success, Error for more than 2 args
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int count;
 	int num;
@@ -20,7 +21,7 @@ int main (int argc, char *argv[])
 	for (count = 1; count < argc; count++)
 	{
 		num = atoi(argv[count]);
-		if (num == '\0' )
+		if (num == '\0')
 		{
 			printf("Error\n");
 			return (1);
