e1ff855513c8f7f6203d250c83204fde3ac10c9c
Kaylee Dao <3597@holbertonschool.com>
2021-10-17 07:08:50 -0700

    Betty Happy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 # include <stdlib.h>
 # include <stdio.h>
-# include "main.h"
 /**
  * main - This program prints it's name
  * @argc: Number of args
  * @argv: The arguments passed
- * Return - 0 for success
+ * Return: 0 for success
  */
 
-int main (__attribute__((unused)) int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
