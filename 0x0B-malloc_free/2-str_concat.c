42bac72e11fd486b2d0bd92627e7f006093f5daa
Kaylee Dao <3597@holbertonschool.com>
2021-10-20 11:35:26 -0700

    Add null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,13 +16,13 @@ char *str_concat(char *s1, char *s2)
 	int size2;
 	int totalsize;
 
-	if (!s1)
+	if (*s1 == '\0')
 	{
-		s1 = '\0';
+		s1 = "";
 	}
-	if (!s2)
+	if (*s2 == '\0')
 	{
-		s2 = '\0';
+		s2 = "";
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
