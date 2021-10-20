33d14ef0c94a78e8bdedf39328abce58718cfe03
Kaylee Dao <3597@holbertonschool.com>
2021-10-20 16:46:33 -0700

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include "main.h"
+# include <string.h>
+/**
+ * free_grid - frees 2D grid space
+ * @grid: grid pointer
+ * @height: grid height
+ * Return: Null if width/height is neg or 0 or pointer
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+	return (0);
+}
