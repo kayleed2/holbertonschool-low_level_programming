8ad927115dcea4c74b594175c9df83bcfbfc8429
Kaylee Dao <3597@holbertonschool.com>
2021-10-20 12:22:58 -0700

    NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,14 @@ char *str_concat(char *s1, char *s2)
 	int size2;
 	int totalsize;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		size1 = i + 1;
@@ -28,23 +36,15 @@ char *str_concat(char *s1, char *s2)
 	p = malloc(totalsize * sizeof(s1[i]));
 	if (!p)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	for (i = 0; i < size1; i++)
 	{
 		p[i] = s1[i];
-		if (s1[0] == '\0')
-		{
-			p = s2;
-		}
 	}
 	for (i = 0; i <= size2; i++)
 	{
 		p[i + size1] = s2[i];
-		if (s2[0] == '\0')
-		{
-			p = s1;
-		}
 	}
 	return (p);
 }
