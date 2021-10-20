ea0d1300c09a62319326ade120528fd1af3e031c
Kaylee Dao <3597@holbertonschool.com>
2021-10-20 07:47:24 -0700

    Put the value inside

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0)
 	{
-		return('\0');
+		return ('\0');
 	}
 	p = malloc(size * sizeof(c));
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
 	return (p);
 }
