c980f8d25fc7c313f01d3c9c5b8ddc37b06c8ff5
Kaylee Dao <3597@holbertonschool.com>
2021-10-20 08:16:05 -0700

    p[i]=str[i]

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include "main.h"
+/**
+ * _strdup - This function creates an array
+ * @str: string to be copied
+ * Return: pointer to array, Null if str is null
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i;
+	unsigned int size;
+
+	if (!str)
+	{
+		return ('\0');
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size = i + 1;
+	}
+	p = malloc(size * sizeof(str[i]));
+	if (!p)
+	{
+		return ('\0');
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
