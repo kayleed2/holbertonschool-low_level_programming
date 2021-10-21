7b29557588056e2eed555cfaaf67b0f26bc8fd82
Kaylee Dao <3597@holbertonschool.com>
2021-10-21 11:50:14 -0700

    Betty doc

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - prints current file
  * This prints current file name
- **/
+ * Return: 0 for success
+**/
 
-int main (void)
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
