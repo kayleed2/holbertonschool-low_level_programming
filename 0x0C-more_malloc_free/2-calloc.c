d7effe3970a1fef4bc17d5253b27bbaa4df0eff7
Kaylee Dao <3597@holbertonschool.com>
2021-10-25 12:30:18 -0700

    No cast

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *p = (unsigned int *)malloc(nmemb * size);
+	unsigned int *p = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 	{
