eb5d4d3b75ad9f334f9d2a1e67af46d40394532d
Kaylee Dao <3597@holbertonschool.com>
2021-10-26 10:50:25 -0700

    Negative

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,20 +10,18 @@
 
 void print_dog(struct dog *d)
 {
-	char *n = "(nil)";
-
 	if (!d)
 		return;
 	if (d->name == NULL)
-		printf("Name: %s\n", n);
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: %s\n", n);
+	if (d->age < 0)
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: %s\n", n);
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
