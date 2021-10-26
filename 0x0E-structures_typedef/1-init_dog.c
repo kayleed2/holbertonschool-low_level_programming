a77ee1740b4ed3d6eba22d4dd7f693808ef23991
Kaylee Dao <3597@holbertonschool.com>
2021-10-26 08:21:51 -0700

    Updated dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "dog.h"
+/**
+ * init_dog - Initialize var
+ * @d: Var of struct dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
