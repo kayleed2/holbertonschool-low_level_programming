b76a96f6fae1c169ae99d4a67eb968b708093c2c
Kaylee Dao <3597@holbertonschool.com>
2021-10-26 12:24:53 -0700

    Another pointer

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	dog_t *cpy;
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
@@ -20,5 +21,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new->name = name;
 	new->age = age;
 	new->owner = owner;
+	cpy = malloc(sizeof(dog_t));
+	cpy->name = name;
+	cpy->owner = owner;
 	return (new);
 }
