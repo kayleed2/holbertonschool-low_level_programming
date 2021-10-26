ab056469e9a05d8f29f54e5ce199d7bc1be3f82d
Kaylee Dao <3597@holbertonschool.com>
2021-10-26 10:25:44 -0700

    Free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "dog.h"
+/**
+ * free_dog - Free the dog
+ * @d: Var of struct dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
