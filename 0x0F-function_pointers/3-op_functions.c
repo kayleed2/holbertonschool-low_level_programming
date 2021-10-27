215f434273276f549a15f3eac2cf551c239ef410
Kaylee Dao <3597@holbertonschool.com>
2021-10-27 09:10:24 -0700

    Creating op functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,27 +2,56 @@
 #include <stdlib.h>
 #include "calc.h"
 /**
- * op_add - Adds two ints 
+ * op_add - Adds two ints
  * @a: int 1
  * @b: int 2
- * Returns: Sum
+ * Return: Sum
  */
 int op_add(int a, int b)
 {
+	return (a + b);
 }
 
+/**
+ * op_sub - Subtracts two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: Difference
+ */
 int op_sub(int a, int b)
 {
+	return (a - b);
 }
 
+/**
+ * op_mul - Multiplies two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: Product
+ */
 int op_mul(int a, int b)
 {
+	return  (a * b);
 }
 
+/**
+ * op_div - Divides two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: Result
+ */
 int op_div(int a, int b)
 {
+	return (a / b);
 }
 
+/**
+ * op_mod - Mods two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: Remainder
+ */
 int op_mod(int a, int b)
 {
+	return (a % b);
 }
