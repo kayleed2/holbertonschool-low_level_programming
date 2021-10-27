42fd7d7c5ca78a7091f515b19ee755a2aff65b07
Kaylee Dao <3597@holbertonschool.com>
2021-10-27 10:42:33 -0700

    First try

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * get_op_func - Calls correct operation function
+ * @s: Operator passed as argument
+ * Return: Pointer to correct op function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i != NULL)
+	{
+		if (s == ops[i])
+		{
+			return (*(ops[i + 1]));
+		}
+	}
+	return (NULL);
+}
