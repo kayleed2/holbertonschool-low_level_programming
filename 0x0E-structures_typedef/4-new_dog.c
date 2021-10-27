5594930633d78a92eea2390b34662a03d75afea7
Kaylee Dao <3597@holbertonschool.com>
2021-10-27 06:25:33 -0700

    Free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 	{
-		free(new->name);
-		free(new->owner);
 		free(new);
 		return (NULL);
 	}
