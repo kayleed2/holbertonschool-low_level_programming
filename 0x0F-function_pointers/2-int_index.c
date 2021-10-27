e07cb07ca873c420bafe5ca9a37d4bcad47c89d4
Kaylee Dao <3597@holbertonschool.com>
2021-10-27 10:31:58 -0700

    Accounting for null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -22,10 +22,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) != 0)
 		{
 			return (i);
-			if (!cmp(array[i]))
-				{
-					return ('\0');
-				}
 		}
 	}
 	return (-1);
