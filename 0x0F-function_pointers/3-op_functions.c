b746eae0fbc42df5f49e653d756f328997243445
Kaylee Dao <3597@holbertonschool.com>
2021-10-27 09:00:02 -0700

    Unfinished

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * op_add - Adds two ints 
+ * @a: int 1
+ * @b: int 2
+ * Returns: Sum
+ */
+int op_add(int a, int b)
+{
+}
+
+int op_sub(int a, int b)
+{
+}
+
+int op_mul(int a, int b)
+{
+}
+
+int op_div(int a, int b)
+{
+}
+
+int op_mod(int a, int b)
+{
+}
