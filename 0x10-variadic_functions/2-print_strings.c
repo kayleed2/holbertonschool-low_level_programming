4603867fccb7d9ced32d8e2d1bfafd065ee428e7
Kaylee Dao <3597@holbertonschool.com>
2021-10-29 10:07:03 -0700

    Accounting for nil

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "variadic_functions.h"
+# include <stdarg.h>
+/**
+ * print_strings - Prints all strings
+ * @separator: String between args
+ * @n: Num of args
+ * Return: Print strings
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(args, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(args, char *));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
