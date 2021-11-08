2bc14007d56776387e83a52dc1a800714c8f9cd6
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 14:04:26 -0800

    Bach to normal

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,28 +18,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/**
-* _strlen - Prints restates var value
-* @s: String to determine length
-* Description: The above
-* Return: Return value should be void  indicating success
-*/
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-
-
-	return (i);
-}
-
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
