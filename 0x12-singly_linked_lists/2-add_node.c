91b964c7c29f8a418cbb351d1c37f8eb427f22d4
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 14:09:08 -0800

    _strlen in file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,3 +31,25 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new;
 	return (new);
 }
+
+/**
+* _strlen - Prints restates var value
+* @s: String to determine length
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+
+	return (i);
+}
