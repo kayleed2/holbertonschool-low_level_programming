8305fafca61ba56a91facc165afc9b982f56d0f5
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 14:05:49 -0800

    _strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * add_node - Adds new node to list_t
  * @head: Pointer to new node for list
@@ -14,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	char *dup = strdup(str);
-	unsigned int n = strlen(dup);
+	unsigned int n = _strlen(dup);
 
 	if (!head || !str)
 		return (NULL);
