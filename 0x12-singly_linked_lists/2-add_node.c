b341a872daadfc59a668438efd7f489fda7dc998
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 09:18:39 -0800

    New Head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Adds new node to list_t
+ * @head: Pointer to new node for list
+ * @str: Data to add
+ * Return: Address of new element
+ **/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *dup = strdup(str);
+	unsigned int n = strlen(dup);
+
+	new = (list_t *) malloc(sizeof(list_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = dup;
+	new->len = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
