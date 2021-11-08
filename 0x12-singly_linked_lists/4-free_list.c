06b40b0bd70253384968b281535d39ee74b54362
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 10:23:26 -0800

    First try

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - Frees list_t
+ * @head: Pointer to list
+ * Return: void
+ **/
+
+void free_list(list_t *head)
+{
+	list_t *next;
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
