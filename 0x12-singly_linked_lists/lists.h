f12d2e98360ea8155f198fb788a30a8fcda279ef
Kaylee Dao <3597@holbertonschool.com>
2021-11-08 14:01:24 -0800

    _strlen

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,10 +18,33 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/**
+* _strlen - Prints restates var value
+* @s: String to determine length
+* Description: The above
+* Return: Return value should be void  indicating success
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+
+	return (i);
+}
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(char *s);
 #endif
