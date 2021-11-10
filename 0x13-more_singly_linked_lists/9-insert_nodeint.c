3a219d71aeb2fc5399beaa79459cf801ebbe7497
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 10:48:01 -0800

    Set temp->next, but still haven't dealt with no match

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * insert_nodeint_at_index - Inserts node at index listint_t
+ * @head: Pointer to list
+ * @idx: Index of list
+ * @n: Data to be added
+ * Return: Sum on all data
+ **/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head;
+	listint_t *new = malloc(sizeof(listint_t));
+	unsigned int index = 1;
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	while (temp)
+	{
+		if (index == idx)
+		{
+			new->next = temp->next;
+			temp->next = new;
+		}
+		temp = temp->next;
+		index++;
+	}
+	return (new);
+}
