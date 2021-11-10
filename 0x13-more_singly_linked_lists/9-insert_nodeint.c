1d650491141c22f1a51f34a0aa84c9bf940ff037
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 11:43:32 -0800

    If index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = (*head)->next;
+		*head = new;
+	}
 	while (temp)
 	{
 		if (index == idx)
