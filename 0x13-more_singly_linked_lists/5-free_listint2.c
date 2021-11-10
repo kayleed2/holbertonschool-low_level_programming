0f387f34b2e3ff13094b5d7113b87151d37fd07b
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 09:08:05 -0800

    Free tmp

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_listint2 - Frees listint_t
+ * @head: Pointer to list
+ * Return: Void
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((*head)->next != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	free(tmp);
+	*head = NULL;
+}
