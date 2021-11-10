5de5439678db985534c0703ff8645d7851786789
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 06:47:30 -0800

    Initialize i

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_listint - Adds new node to list_t
+ * @h: Pointer to list
+ * Return: Number of nodes
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *ptr = h;
+
+	if (!h)
+		return (-1);
+	while (ptr)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
