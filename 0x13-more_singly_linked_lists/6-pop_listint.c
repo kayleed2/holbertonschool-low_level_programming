2c302ad645c9a6c10d46c953e5821410d494b7dc
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 09:17:29 -0800

    Saving n

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * pop_listint - Deletes head node listint_t
+ * @head: Pointer to list
+ * Return: Head node's data or 0
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp = (*head)->next;
+	int n = (*head)->n;
+
+	free(*head);
+	*head = tmp;
+	return (n);
+}
