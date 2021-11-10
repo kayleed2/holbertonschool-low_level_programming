d9f4973abc435f16145420e926d7beef153aad5d
Kaylee Dao <3597@holbertonschool.com>
2021-11-10 09:27:11 -0800

    Iterate n and compare to index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * get_nodeint_at_index - Deletes head node listint_t
+ * @head: Pointer to list
+ * @index: Index of node
+ * Return: Nth node
+ **/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+
+	while (head)
+	{
+		head = head->next;
+		n++;
+		if (n == index)
+			return (head);
+	}
+	return (NULL);
+}
