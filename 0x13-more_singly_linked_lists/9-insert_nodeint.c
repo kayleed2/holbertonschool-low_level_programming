2e535cc161f50080f7aa540d49fd5cb31685b8c5
Kaylee Dao <3597@holbertonschool.com>
2021-11-11 06:25:46 -0800

    If insert fails

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,5 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		index++;
 	}
+	if (temp == NULL && index != idx - 1)
+		return (NULL);
 	return (new);
 }
