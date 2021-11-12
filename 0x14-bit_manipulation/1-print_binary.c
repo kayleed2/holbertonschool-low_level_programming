a5bc79f4d195d2db4868bf0ea41b09fb622dc3fb
Kaylee Dao <3597@holbertonschool.com>
2021-11-12 15:25:53 -0800

    Recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,9 @@
 
 void print_binary(unsigned long int n)
 {
-	int c, k;
-
-	for (c = 31; c >= 0; c--)
+	if (n > 1)
 	{
-		k = n >> c;
-		if (k & 1)
-			_putchar('1');
-		else
-			_putchar('0');
+		print_binary(n >> 1);
 	}
-	_putchar('\n');
+	_putchar((n & 1) + '0');
 }
