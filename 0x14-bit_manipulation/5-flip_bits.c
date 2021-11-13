9de76be1b07988208d2a4d86bbc603f1700f3819
Kaylee Dao <3597@holbertonschool.com>
2021-11-12 17:30:41 -0800

    XOR then count 1s

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * flip_bits - From one num to another
+ * @n: Num given
+ * @m: Num to convert to
+ * Return: Number of bits to flip
+ **/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit;
+	unsigned int i = 0;
+
+	if (!n || !m)
+	{
+		return (-1);
+	}
+	bit = n ^ m;
+	while (bit)
+	{
+		i = i + (bit & 1);
+		bit = bit >> 1;
+	}
+	return (i);
+}
