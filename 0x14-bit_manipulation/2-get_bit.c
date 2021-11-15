6083646d95422e9354faa5e6a75ca6ca199a1173
Kaylee Dao <3597@holbertonschool.com>
2021-11-15 12:56:42 -0800

    If / Else

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,8 +13,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
-	if (index < 0)
-		return (-1);
+	if (index > 0)
 	bit = (n >> index) & 1;
+	else
+		return (-1);
 	return (bit);
 }
