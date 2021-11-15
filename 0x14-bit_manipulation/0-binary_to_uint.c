07124c01aa5ac6014f487b7e5964621390db6739
Kaylee Dao <3597@holbertonschool.com>
2021-11-15 13:05:54 -0800

    Dereference b

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int len = strlen(b);
 
-	if (b == NULL)
+	if (b == NULL || *b == NULL)
 		return (0);
 	for (i = len - 1; i >= 0; i--)
 	{
