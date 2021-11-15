b13b3c4cdb6318dba795fd51780c6d6c58dcfa01
Kaylee Dao <3597@holbertonschool.com>
2021-11-15 13:22:42 -0800

    Rid of Unnecessary strlen

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,6 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	unsigned int x = 1;
 	int i = 0;
 
 	if (!b)
@@ -19,10 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
-		{
-			num = num + (b[i] - '0') * x;
-			x = x * 2;
-		}
+			num = ((num * 2) + (b[i] - '0'));
 		else
 			return (0);
 	}
