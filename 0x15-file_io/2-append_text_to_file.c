27e77de52fa30154d2f80c03bdf506cca41172b1
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 11:28:56 -0800

    First try

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * append_text_to_file - Appends to a file
+ * @filename: Filename
+ * @text_content: String to write to file
+ * Return: 1 for success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdopen;
+	int len = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fdopen = open(filename, O_RDWR | O_APPEND);
+
+	if (text_content == NULL && fdopen == -1)
+		return (-1);
+
+	if (text_content == NULL && fdopen == 1)
+		return (1);
+
+	if (fdopen == -1)
+		return (-1);
+
+	write(fdopen, text_content, len);
+	return (1);
+}
