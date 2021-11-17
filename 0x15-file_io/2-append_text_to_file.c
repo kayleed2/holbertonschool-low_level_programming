561a57e23080e2fbcf3c4e10572c7e7ce4e9a955
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 14:42:57 -0800

    Check write

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fdopen;
+	int fdopen, w;
 
 	if (filename == NULL)
 		return (-1);
@@ -32,6 +32,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fdopen == -1)
 		return (-1);
 
-	write(fdopen, text_content, strlen(text_content));
+	w = write(fdopen, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
 	return (1);
 }
