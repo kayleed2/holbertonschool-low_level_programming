52ba0ddd30b83696b6f35b8188cc64ce465d5762
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 14:33:30 -0800

    Check write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fdopen;
+	int fdopen, w;
 	int len = strlen(text_content);
 
 	if (filename == NULL)
@@ -30,6 +30,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	write(fdopen, text_content, len);
+	w = write(fdopen, text_content, len);
+	if (w == -1)
+		return (-1);
 	return (1);
 }
