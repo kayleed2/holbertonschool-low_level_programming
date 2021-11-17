55818a3e05f57511877e783755382c4aa14cf294
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 15:58:42 -0800

    Return value

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fdfile1, fdfile2, w, cl, nchars = 1024;
+	int fdfile1, fdfile2, cl, nchars = 1024;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -26,16 +26,14 @@ int main(int argc, char *argv[])
 	}
 	fdfile1 = open(argv[1], O_RDONLY);
 	fdfile2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	while (nchars == 1024)
+	while ((nchars = read(fdfile1, buffer, 1024)) != 0)
 	{
-		nchars = read(fdfile1, buffer, 1024);
 		if (nchars == -1 || fdfile1 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		w = write(fdfile2, buffer, nchars);
-		if (fdfile2 == -1 || w == -1)
+		if (write(fdfile2, buffer, nchars) != nchars)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -53,5 +51,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile2);
 		exit(100);
 	}
-	return (1);
+	return (0);
 }
