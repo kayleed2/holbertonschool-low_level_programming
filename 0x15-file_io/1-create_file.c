6cb860ed8f361e96fdab2ffbf841ff0a78f6f029
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 11:33:22 -0800

    Check fd value

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,9 @@ int create_file(const char *filename, char *text_content)
 
 	fdopen = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 
+	if (fdopen == -1)
+		return (-1);
+
 	if (text_content == NULL)
 		return (1);
 
