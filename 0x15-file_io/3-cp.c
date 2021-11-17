60c03a3da8189838e0a01c58ef47976d4d802e19
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 15:25:19 -0800

    While loop for write

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,8 +16,8 @@
 
 int main(int argc, char *argv[])
 {
-	int fdfile1, fdfile2, r, w, cl;
-	char *buffer[1024];
+	int fdfile1, fdfile2, w, cl, nchars = 1024;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -25,24 +25,27 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fdfile1 = open(argv[1], O_RDONLY);
-	r = read(fdfile1, buffer, 1024);
-	if (fdfile1 == -1 || r == -1 || !argv[1])
+	fdfile2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	while (nchars == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		nchars = read(fdfile1, buffer, 1024);
+		if (nchars == -1 || fdfile1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		w = write(fdfile2, buffer, nchars);
+		if (fdfile2 == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	cl = close(fdfile1);
 	if (cl == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile1);
 		exit(100);
-	}
-	fdfile2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
-	w = write(fdfile2, buffer, 1024);
-	if (fdfile2 == -1 || w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
 	}
 	cl = close(fdfile2);
 	if (cl == -1)
