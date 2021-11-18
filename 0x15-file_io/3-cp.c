5fd8765cad8e2ee157e704134c1212993c790426
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 16:39:22 -0800

    Close

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 			exit(98);
 	fdfile2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fdfile2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]),
 			exit(98);
 	while (nchars == 1024)
 	{
