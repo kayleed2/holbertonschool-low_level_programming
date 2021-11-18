f183490df1d93f304f32d0601a801dcfc0654687
Kaylee Dao <3597@holbertonschool.com>
2021-11-17 16:27:09 -0800

    Betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,40 +16,33 @@
 
 int main(int argc, char *argv[])
 {
-	int fdfile1, fdfile2, cl, nchars = 1024;
+	int fdfile1, fdfile2, nchars = 1024;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fdfile1 = open(argv[1], O_RDONLY);
+	if (fdfile1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	fdfile2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fdfile2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	while ((nchars = read(fdfile1, buffer, 1024)) != 0)
 	{
-		if (nchars == -1 || fdfile1 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		if (nchars == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
 		if (write(fdfile2, buffer, nchars) != nchars)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+				exit(99);
 	}
-	cl = close(fdfile1);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile1);
-		exit(100);
-	}
-	cl = close(fdfile2);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile2);
-		exit(100);
-	}
+	if (close(fdfile1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile1),
+			exit(100);
+	if (close(fdfile2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfile2),
+			exit(100);
 	return (0);
 }
