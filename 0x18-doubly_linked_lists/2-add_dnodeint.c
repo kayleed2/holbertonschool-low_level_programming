0c7e964bf0ceac25fe54015bc665fd4b26b757f9
Kaylee Dao <3597@holbertonschool.com>
2021-12-07 09:49:26 -0800

    Free if NULL

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 
 	if (!newnode)
+	{
+		free(newnode)
 		return (NULL);
+	}
 
 	newnode->n = n;
 	newnode->prev = NULL;
