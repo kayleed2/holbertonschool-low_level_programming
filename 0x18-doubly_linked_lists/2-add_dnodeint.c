32124c6becbdaf23b748974edd664c1d3f9f0782
Kaylee Dao <3597@holbertonschool.com>
2021-12-07 08:44:28 -0800

    Return NULL on failure

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 
+	if (!newnode)
+		return (NULL);
+
 	newnode->n = n;
 	newnode->prev = NULL;
 	newnode->next = *head;
