eb8b16689d49dbd0b0092b7bc79f3a75050e318b
Kaylee Dao <3597@holbertonschool.com>
2021-12-08 06:38:41 -0800

    Check if index with iterator

---
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Finds nth node of list
+ * @head: List passed in
+ * @index: nth node
+ * Return: Returns nth node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		head = head->next;
+		i++;
+		if (i == index)
+			return (head);
+	}
+	return (NULL);
+}
