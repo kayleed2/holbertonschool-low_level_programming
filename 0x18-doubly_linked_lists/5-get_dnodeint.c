5bad2daad17be3437f7f9b915f36f0445c8cc33b
Kaylee Dao <3597@holbertonschool.com>
2021-12-08 06:49:48 -0800

    Back to OG

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL || index == NULL)
+	if (head == NULL || !index)
 		return (NULL);
 
 	while (head)
