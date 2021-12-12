7c03834bd78a21746304f48c4f39961031105d07
Kaylee Dao <3597@holbertonschool.com>
2021-12-12 13:30:42 -0800

    Back to OG

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,14 +10,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL)
 		return (NULL);
 
-	while (ht->array[index])
-	{
-		if (strcmp(key, ht->array[index]->key) == 0)
-			return (ht->array[index]->value);
-		ht->array[index] =  ht->array[index]->next;
-	}
-	return (NULL);
+	if (!ht->array[index])
+ 		return (NULL);
+	return (ht->array[index]->value);
 }
