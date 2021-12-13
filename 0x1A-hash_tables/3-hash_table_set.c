96570d8f34cfa47d252996127bde26616c9032f9
Kaylee Dao <3597@holbertonschool.com>
2021-12-13 08:48:06 -0800

    Moving key/index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,12 +9,14 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_element, *element = ht->array[index];
+	unsigned long int index;
+	hash_node_t *new_element, *element;
 
 	if (key == NULL)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	element = ht->array[index];
 	new_element = malloc(sizeof(hash_node_t) * 1);
 
 	if (!new_element)
