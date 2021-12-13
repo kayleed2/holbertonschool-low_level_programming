71c3cb22eed711d00e49ea86bc42996e7c01abbb
Kaylee Dao <3597@holbertonschool.com>
2021-12-12 16:07:24 -0800

    Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!new_element)
 		return (0);
-	new_element->key = strdup(key);;
+	new_element->key = strdup(key);
 	new_element->value = strdup(value);
 	new_element->next = NULL;
 
