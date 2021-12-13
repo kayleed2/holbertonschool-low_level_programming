1a7b69b5abd54189830fb0d0b3bf18ea07ce49e7
Kaylee Dao <3597@holbertonschool.com>
2021-12-13 08:53:24 -0800

    Return malloc NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_element, *element;
 
-	if (key == NULL)
+	if (key == NULL || !ht)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
