a1d0e1d49cc9d537f8930c2eca99a6eedd5df47d
Kaylee Dao <3597@holbertonschool.com>
2021-12-13 08:41:37 -0800

    Switching setting tmp and index

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,14 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *tmp;
 
-	if (key == NULL || !ht)
+	if (!ht)
 		return (NULL);
 
-	tmp = ht->array[index];
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (strcmp(tmp->key, key) == 0)
-		return (tmp->value);
+	tmp = ht->array[index];
 
 	while (tmp != NULL)
 	{
