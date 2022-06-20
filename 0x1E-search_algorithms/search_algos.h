6193c1723a59371193726f6cf8474c1f22d30873
Kaylee Dao <kayleed2@gmail.com>
2022-06-20 08:45:53 -0500

    Adding advanced binary prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
+void print_array(const int *array, size_t size);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
