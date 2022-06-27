a34260a1d127543bffd62706c3d149614ce5e446
Kaylee Dao <kayleed2@gmail.com>
2022-06-27 11:03:24 -0500

    Updated header file

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+size_t i;
+
+i = 0;
+while (array && i < size)
+{
+if (i > 0)
+printf(", ");
+printf("%d", array[i]);
+++i;
+}
+printf("\n");
+}
+
+/**
+* linear_skip - Searches for value in sorted array
+* @array: Pointer to 1st element
+* @size: Number of elements
+* @value: Value to search for
+* Return: Index of value or -1 if NULL
+**/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
 int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
